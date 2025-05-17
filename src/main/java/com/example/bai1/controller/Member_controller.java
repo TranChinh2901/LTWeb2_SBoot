e0a2256edd0ff0978db075f2c8afd730af5fddc7
TranChinh2901 <tranchinht32901@gmail.com>
2025-05-17 14:06:57 +0700

    pussh code

--- src/main/java/com/example/bai1/controller/Member_controller.java
+++ src/main/java/com/example/bai1/controller/Member_controller.java
@@ -4,21 +4,29 @@ import com.example.bai1.model.Member;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.Arrays;
 import java.util.List;
 
 @Controller
 public class Member_controller {
+    List<Member> members = Arrays.asList(
+            new Member("Trần Viết Chính",1,21,"chinh99078@donga.edu.vn"),
+            new Member("Thái Nguyễn Văn Ngocj",2,21,"Ngoc98400@donga.edu.vn"),
+            new Member("Phạm Minh Vũ",3,21,"vu98447@donga.edu.vn"),
+            new Member("Ngô Bá Quang", 4, 21,"quang98261@donga.edu.vn")
+    );
     @GetMapping("/members")
     public String showMembers(Model model) {
-        List<Member> members = Arrays.asList(
-                new Member("Trần Viết Chính"),
-                new Member("Thái Nguyễn Văn Ngọc "),
-                new Member("Ngô Bá Quang"),
-                new Member("Phạm Minh Vũ")
-        );
         model.addAttribute("members", members);
         return "show_members";
+    }
+
+    @GetMapping("/members/{id}")
+    public String showMember(@PathVariable int id, Model model) {
+        Member found = members.stream().filter(m -> m.getId() == id).findFirst().orElse(null);
+        model.addAttribute("member", found);
+        return "detail_member";
     }
 }
