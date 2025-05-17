33202cf85b4679b76054bc5e7558f5c4d83ccc47
TranChinh2901 <tranchinht32901@gmail.com>
2025-05-17 11:30:56 +0700

    cau hinh

---
+++ src/main/java/com/example/bai1/controller/Member_controller.java
@@ -0,0 +1,22 @@
+package com.example.bai1.controller;
+
+import com.example.bai1.model.Member;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Member_controller {
+    @GetMapping("/members")
+    public String showMembers(Model model) {
+        List<Member> members = Arrays.asList(
+                new Member("Trần Viết Chính"),
+                new Member("Thái Nguyễn Văn Ngọc "),
+                new Member("Ngô Bá Quang"),
+                new Member("Phạm Minh Vũ")
+        );
+        model.addAttribute("members", members);
+        return "printMember"; // Trả về file templates/members.html
+    }
+}
